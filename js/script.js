ec53013cff171defe173b8a5f3e4d43d2187a5d5
MichaelCarlosAlvarez <michaelalvarez777@gmail.com>
2024-02-21 18:48:33 -0800

    final Commit
    Finished my first Assignment!

---
+++ js/script.js
@@ -0,0 +1,106 @@
+//STEP 1 
+//let someMonth function theMonth() let currentMonth let summerMonth let myLibraryFunction
+//Step 2 
+//6 "Hello" True null 
+//STEP 3 
+// let y = 8 + 2, let X=5
+//STEP 4
+  /*
+let strFirstName;
+let strLastName;
+let intAddress;
+let strCity;
+let strState;
+let intZipCode;
+let intYourAge;
+let strReferralSource;
+let blnMayWeContactYou;
+*/
+//STEP 5
+  /*let strFirstName = "Michael";
+let strState; 
+strState = "California";
+let intZipCode = 91210, strCity = "Chicago";
+//STEP 6
+let ageTerm = "Zack is " + 7;
+console.log(ageTerm);*/
+//Step 7
+/*let name = true + "Michael";
+console.log(name);
+let number = 7 + false;
+console.log(number);*/
+//STEP 8 No. I would add a backslash for I'm. So it would say I\'m
+//STEP 9 
+/*let y = null;
+let x;
+console.log(x);
+console.log(y);*/
+//STEP 10 
+//console.log(typeof 'sup', typeof 7, typeof true, typeof Object, typeof r)
+//STEP 11
+/*
+alert('Hello '+ 'Michael Alvarez' + ', weclome to the JavaScript class!');
+*/
+//STEP 12
+/*
+let name = "Michael Alvarez";
+alert('Hello '+ name + ', weclome to the JavaScript class!');
+*/
+//STEP 13
+/*
+let name = "Michael Alvarez";
+let course = "JavaScript";
+alert('Hello ' + name + ', welcome to the ' + course + ' class!')
+*/
+//STEP 14 
+/*
+let name = "Michael Alvarez";
+let course = "JavaScript";
+alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!')
+*/
+//STEP 15
+/*
+let name = prompt("What's ur name bro?")
+let course = "JavaScript";
+alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!')
+*/
+//STEP 16 
+/*
+let name = prompt("What's ur name bro?")
+let course = prompt("And what class are you taking? Asking for a friend.")
+alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!')
+*/
+//STEP 17
+/*
+let x = 10;
+let y = 20;
+console.log(x + y);
+*/
+//STEP 18
+/*
+let x = 20;
+console.log(x += 20);
+*/
+// STEP 19
+/*
+let x = 20;
+console.log(x *=5);
+*/
+// STEP 20
+/*
+let x = 20 % 3;
+console.log(x /= 1);
+*/
+//STEP 21
+/*
+let x = 4;
+let y =4;
+let z = y == 4 && y == x;
+console.log(z)
+*/
+//STEP 22
+/*
+let y = 7;
+let z = y > 7 || y < 7;
+console.log(z);
+*/
